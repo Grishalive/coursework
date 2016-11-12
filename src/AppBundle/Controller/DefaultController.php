73ffced6a029a2ce849b223c5c4d1d8a42f3273f
Grisha Ptushko <grisha.live.forever@gmail.com>
2016-11-13 00:17:27 +0300

    Added simple login and registration

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('registration/index.html.twig', [
+        return $this->render('security/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
